8f169d7e10cee21852d4235b61ea5395978c24bb
Melissa Price <melissaashprice@gmail.com>
2019-05-31 10:10:10 -0400

    restructured & added footer

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -4,24 +4,29 @@ import React, { Component } from 'react';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
-				<div style={{ height: '100vh', position: 'relative',  marginBottom: '-24px',backgroundColor: '#343'}}>
-					<Layout 
+				<div
+					style={{
+						height: '100vh',
+						position: 'relative',
+						marginBottom: '-24px',
+						backgroundColor: '#343'
+					}}
+				>
+					<Layout
 						style={{
 							background:
-								'url(https://images.unsplash.com/photo-1505867104426-a08b9a21aae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80) center / cover', opacity: '.88'
+								'url(https://images.unsplash.com/photo-1505867104426-a08b9a21aae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80) center / cover',
+							opacity: '.88'
 						}}
 					>
-						<Header
-							transparent
-							title=" "
-							style={{ color: 'white' }}
-						>
-							<Navigation className='fScreen-Nav'>
+						<Header transparent title=" " style={{ color: 'white' }}>
+							<Navigation className="fScreen-Nav">
 								<a href="#">About</a>
 								<a href="#">Projects</a>
 								<a href="#">Contact</a>
@@ -36,24 +41,29 @@ class App extends Component {
 							</Navigation>
 						</Drawer> */}
 						<div className="highlight">
-						<div className="highlight-heading">
-						<h1>Melissa Price</h1>
-						</div>
-						<div className="highlight-subtexted">
-						<p>Front-End Developer 
-							<span className='pipes'>|</span> 
-							 Creator 
-							<span className='pipes'>|</span> 							
-							 Music Enthusiast </p>
-						</div>
-						<button className='mdl-button mdl-js-button mdl-button--raised action-btn view-projects'>View Projects</button>	
+							<div className="highlight-heading">
+								<h1>Melissa Price</h1>
+							</div>
+							<div className="highlight-subtexted">
+								<p>
+									Front-End Developer
+									<span className="pipes">|</span>
+									Creator
+									<span className="pipes">|</span>
+									Music Enthusiast{' '}
+								</p>
+							</div>
+							<button className="mdl-button mdl-js-button mdl-button--raised action-btn view-projects">
+								View Projects
+							</button>
 						</div>
 						{/* <Content /> */}
 					</Layout>
 				</div>
 				<About />
 				<Projects />
-        <Contact/>
+				<Contact />
+				<Footer />
 			</div>
 		);
 	}

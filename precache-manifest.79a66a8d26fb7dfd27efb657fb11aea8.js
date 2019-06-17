ee20ffc95e1650d259fe4b770f214f7f520d329f
Melissa Price <melissaashprice@gmail.com>
2019-06-17 18:45:23 -0400

    Updates

---
+++ precache-manifest.79a66a8d26fb7dfd27efb657fb11aea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b800ad72f7cf89684cacdd2f6ee11106",
+    "url": "/Mel-Portfolio/index.html"
+  },
+  {
+    "revision": "9f8ed0efb43a63692be5",
+    "url": "/Mel-Portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "cde69b347388ead9576a",
+    "url": "/Mel-Portfolio/static/css/main.2bfbe4f2.chunk.css"
+  },
+  {
+    "revision": "9f8ed0efb43a63692be5",
+    "url": "/Mel-Portfolio/static/js/2.21bc5a41.chunk.js"
+  },
+  {
+    "revision": "cde69b347388ead9576a",
+    "url": "/Mel-Portfolio/static/js/main.3cdc4be4.chunk.js"
+  },
+  {
+    "revision": "8a2c7b9aee5d5854caa7",
+    "url": "/Mel-Portfolio/static/js/runtime~main.710ed75b.js"
+  },
+  {
+    "revision": "358af740a4641b06337cb1bb12fdb225",
+    "url": "/Mel-Portfolio/static/media/Headshot.358af740.jpg"
+  }
+]);
